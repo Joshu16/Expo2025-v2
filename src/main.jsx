9673bbdd97f6cb432ef229713e0340ff4b8bce1e
Joshu16 <jsaborio1604@gmail.com>
2025-08-20 10:45:20 -0600

    Deploy a GitHub Pages: HashRouter, base /Expo-2025/, rutas relativas; agregar gh-pages y scripts de deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './styles/App.css'
 import App from './pages/App.jsx'
 
@@ -10,8 +10,8 @@ document.body.setAttribute('data-theme', savedTheme);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
